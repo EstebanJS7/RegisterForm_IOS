8cf9462c0e518d4f0894750b857b25d76e2803d1
Esteban Jimenez <estebandjs7@gmail.com>
2023-08-17 16:04:39 -0400

    version v1.0

--- RegisterForm_IOS/ViewController.swift
+++ RegisterForm_IOS/ViewController.swift
@@ -45,6 +45,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         genderPickerView.tag = 1
         cvPickerView.tag = 2
+        
+        telTextField.keyboardType = .numberPad
     }
     
     func createToolbar() ->UIToolbar{
@@ -76,6 +78,24 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
     }
     
+    func isValidPhoneNumber(_ phoneNumber: String) -> Bool {
+        let phoneNumberRegex = "^09[0-9]*$"
+        let phoneNumberTest = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
+        return phoneNumberTest.evaluate(with: phoneNumber)
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: email)
+    }
+    
+    func isValidRUC(_ ruc: String) -> Bool {
+        let rucRegex = "^[0-9]+-[0-9]$"
+        let rucTest = NSPredicate(format: "SELF MATCHES %@", rucRegex)
+        return rucTest.evaluate(with: ruc)
+    }
+    
     func todosLosCamposCompletos() -> Bool {
         // Verifica si todos los campos requeridos tienen texto
         let camposRequeridos: [UITextField] = [dateTextField, genderTextField, cvTextField, telTextField, emailTextField, rucTextField]
@@ -96,16 +116,59 @@ class ViewController: UIViewController, UITextFieldDelegate {
         present(alerta, animated: true, completion: nil)
     }
     
+    func mostrarAlertaTelefonoInvalido() {
+        let alerta = UIAlertController(title: "Teléfono inválido", message: "El número de teléfono debe comenzar con '09' y contener solo dígitos.", preferredStyle: .alert)
+        let accionAceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(accionAceptar)
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    func mostrarAlertaEmailInvalido() {
+        let alerta = UIAlertController(title: "Correo inválido", message: "Por favor, ingresa un correo electrónico válido.", preferredStyle: .alert)
+        let accionAceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(accionAceptar)
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    func mostrarAlertaRUCInvalido() {
+        let alerta = UIAlertController(title: "RUC inválido", message: "El RUC debe tener el formato correcto, por ejemplo: 4648961-6.", preferredStyle: .alert)
+        let accionAceptar = UIAlertAction(title: "Aceptar", style: .default, handler: nil)
+        alerta.addAction(accionAceptar)
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    func alertaCampos(email: String, telefono: String, ruc:String) -> Bool {
+        let emailValido = isValidEmail(email)
+        let telefonoValido = isValidPhoneNumber(telefono)
+        let rucValido = isValidRUC(ruc)
+        
+        
+        if !telefonoValido {
+            mostrarAlertaTelefonoInvalido()
+        }
+        if !emailValido {
+            mostrarAlertaEmailInvalido()
+        }
+        if !rucValido{
+            mostrarAlertaRUCInvalido()
+        }
+        return emailValido && telefonoValido && rucValido
+    }
+    
+    
+    
     @IBAction func sendForm(_ sender: UIButton) {
         if todosLosCamposCompletos() {
-                // Aquí puedes realizar el envío del formulario
-                // por ejemplo, enviar datos a un servidor, etc.
-
-                // Mostrar una alerta de éxito
+            let email = emailTextField.text ?? ""
+            let telefono = telTextField.text ?? ""
+            let ruc = rucTextField.text ?? ""
+            
+            if alertaCampos(email: email, telefono: telefono, ruc: ruc) {
                 mostrarAlertaExito()
-            } else {
-                mostrarAlertaCamposFaltantes()
             }
+        } else {
+            mostrarAlertaCamposFaltantes()
+        }
     }
     
 }
