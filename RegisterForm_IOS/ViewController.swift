a603240d9d2fc71a3bde528f8c8f8721140299d9
Esteban Jimenez <estebandjs7@gmail.com>
2023-08-17 09:30:40 -0400

    adds labels, input sand date picker en fecha de nacimiento

--- RegisterForm_IOS/ViewController.swift
+++ RegisterForm_IOS/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    let datePicker = UIDatePicker()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createDatePicker()
+                
     }
-
-
+    
+    func createToolbar() ->UIToolbar{
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
+        toolbar.setItems([doneBtn], animated: true)
+        
+        return toolbar
+    }
+   
+    func createDatePicker(){
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        
+        datePicker.maximumDate = Date()
+        
+        dateTextField.textAlignment = .center
+        dateTextField.inputView = datePicker
+        dateTextField.inputAccessoryView = createToolbar()
+    }
+    
+    @objc func donePressed(){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        self.dateTextField.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
 }
-
